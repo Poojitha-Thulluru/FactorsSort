c3f7d879f0e6f5ed3c2e2a24f97efd6858fe9956
Poojitha-Thulluru <thulluru.poojitha@gmail.com>
2023-07-20 08:06:20 +0530

    updated space comlexity

--- factors_sort.py
+++ factors_sort.py
@@ -1,8 +1,6 @@
-def get_sorted_factors(nums_array: list) -> list:
-    factors_counts = [(num, count_factors(num)) for num in nums_array]
-    factors_counts.sort(key=lambda x: (x[1], x[0]))
-    result = [num for num, _ in factors_counts]
-    return result
+def get_sorted_array(nums_array: list) -> list:
+    nums_array.sort(key=lambda num: (count_factors(num), num))
+    return nums_array
 
 
 def count_factors(num: int) -> int:
@@ -15,6 +13,6 @@ def count_factors(num: int) -> int:
 
 try:
     num_array = list(map(int, input("Enter integers separated by space : ").split()))
-    print("The factors sorted array is : ", get_sorted_factors(num_array))
+    print("The factors sorted array is : ", get_sorted_array(num_array))
 except ValueError:
     print("Invalid Input, Please enter only integer")
